e8ffc76c2976f3bd38eeb639ee5fd3ffcc06a830
marchenkomaxim <songoci222@gmail.com>
2024-07-04 15:45:57 +0300

    Lesson 31

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
 
 Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth', AdminMiddleWare::class, EnsureEmailIsVerified::class]], function () {
     Route::group(['namespace' => 'Main'], function () {
-        Route::get('/', 'IndexController')->name('index');
+        Route::get('/', 'IndexController')->name('admin.main.index');
     });
 
     Route::group(['namespace' => 'Post', 'prefix' => 'posts'], function () {
